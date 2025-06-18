2f613b3bda3b87504f068d1a9e00c1bec69749eb
davidjsm245 <120421579+davidjsm245@users.noreply.github.com>
2025-06-17 21:02:30 -0500

    El proyecto de estructura de Datos

---
+++ src/estructura_de_datos/ESTRUCTURA_DE_DATOS.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package estructura_de_datos;
+
+/**
+ *
+ * @author david
+ */
+public class ESTRUCTURA_DE_DATOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
